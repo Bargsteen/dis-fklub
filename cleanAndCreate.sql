5281f8650989f1b16ce28d160599432eecbfd7f9
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2018-10-10 21:08:39 +0200

    Add fill_store func, structure, todos.

---
+++ cleanAndCreate.sql
@@ -0,0 +1,84 @@
+DROP SCHEMA fct CASCADE;
+
+DROP SCHEMA dim CASCADE;
+
+CREATE SCHEMA fct;
+
+CREATE SCHEMA dim;
+
+CREATE TABLE dim.product (
+    product_id serial PRIMARY KEY,
+
+    -- Properties
+    name varchar(50) NOT NULL,
+    category varchar(50) NOT NULL,
+    price decimal (5,
+        2) NOT NULL,
+    alcohol_content_ml decimal (2,
+        2) NOT NULL,
+
+    -- When it is available for purchase
+    activate_date date NOT NULL,
+    deactivate_date date,
+
+    -- Slowly changing dimension fields
+    version int NOT NULL,
+    valid_from date NOT NULL,
+    valid_to date
+);
+
+CREATE TABLE dim.time(
+    time_id serial PRIMARY KEY,
+
+    -- Properties
+    t_year int NOT NULL,
+    t_month int NOT NULL,
+    t_day int NOT NULL,
+    t_hour int NOT NULL,
+    day_of_week int NOT NULL,
+    is_fall_semester BOOLEAN NOT NULL,
+    is_holiday BOOLEAN NOT NULL
+);
+
+CREATE TABLE dim.store (
+    store_id serial PRIMARY KEY,
+
+    -- Properties
+    name varchar(50) NOT NULL,
+    description varchar(50) NOT NULL,
+
+    -- Slowly changing dimension fields
+    version int NOT NULL,
+    valid_from date NOT NULL,
+    valid_to date
+);
+
+CREATE TABLE dim.member (
+    member_id serial PRIMARY KEY,
+
+    -- Properties
+    gender varchar(1) NOT NULL,
+    is_active boolean NOT NULL,
+    course varchar(50),   
+
+    -- Slowly changing dimension fields
+    version int NOT NULL,
+    valid_from date NOT NULL,
+    valid_to date
+);
+
+CREATE TABLE fct.sale (
+    sale_id serial,
+
+    -- Dimension foreign keys
+    fk_product_id serial NOT NULL,
+    fk_time_id serial NOT NULL,
+    fk_store_id serial NOT NULL,
+    fk_member_id serial NOT NULL,
+
+    -- Degenerate dimension
+    order_id serial NOT NULL,
+
+    -- Measures
+    item_count int NOT NULL
+);
--- etl.py
+++ etl.py
@@ -1,65 +1,209 @@
 import pygrametl
-from pygrametl.datasources import CSVSource, MergeJoiningSource
-from pygrametl.tables import CachedDimension, SlowlyChangingDimension, BulkFactTable
+from pygrametl.datasources import CSVSource, MergeJoiningSource, TypedCSVSource
+from pygrametl.tables import CachedDimension, SlowlyChangingDimension, FactTable
 import psycopg2
 
 import datetime
 import sys
 import time
+import csv
 
-# Connection
+### Connection ###
 pgconn = psycopg2.connect(database="dis", port=5432)
 connection = pygrametl.ConnectionWrapper(pgconn)
 connection.setasdefault()
 connection.execute('set search_path to pygrametlexa')
 
 
-# Methods
-def pgcopybulkloader(name, atts, fieldsep, rowsep, nullval, filehandle):
-    # Here we use driver-specific code to get fast bulk loading.
-    # You can change this method if you use another driver or you can
-    # use the FactTable or BatchFactTable classes (which don't require
-    # use of driver-specifc code) instead of the BulkFactTable class.
-    global connection
-    curs = connection.cursor()
-    curs.copy_from(file=filehandle, table=name, sep=fieldsep,
-                   null=str(nullval), columns=atts)
+
+### Database Cleanup ###
+def executeScriptsFromFile(filename):
+    # WARNING: Does not work as intended.. From Stackoverflow.
+
+    # Open and read the file as a single buffer
+    fd = open(filename, 'r')
+    sqlFile = fd.read()
+    fd.close()
+
+    # all SQL commands (split on ';')
+    sqlCommands = sqlFile.split(';')
+
+    # Execute every command from the input file
+    for command in sqlCommands:
+        # This will skip and report errors
+        # For example, if the tables do not yet exist, this will skip over
+        # the DROP TABLE commands
+        try:
+            connection.execute(command)
+        except Exception as msg:
+            print("Command skipped: ", msg)
 
 
-# Dimensions
-productDim = SlowlyChangingDimension(
-  name="dim.product",
-  key="product_id",
-  attributes=["name", "price", "alcohol_content_ml", 
-  "activate_date", "deactivate_date", "version", "valid_from", "valid_to"],
-  lookupatts="name",
-  versionatt="version",
-  fromatt="valid_from",
-  toatt="valid_to",
-  srcdateatt="lastmoddate",
-  cachesize=-1
+### Files and Sources ###
+
+csv.register_dialect('fklubDialect', delimiter=';', quoting=csv.QUOTE_NONE)
+
+category_file_handle = open('fklubdw/FKlubSourceData/category.csv')
+category_source = TypedCSVSource(category_file_handle,
+                                 {'id': int, 'name': str},
+                                 dialect='fklubDialect')
+
+member_file_handle = open('fklubdw/FKlubSourceData/member.csv')
+member_source = TypedCSVSource(member_file_handle,
+                               {'id': int, 'active': bool, 'year': int,
+                                'gender': str, 'want_spam': bool,
+                                'balance': int, 'undo_count': int},
+                               dialect='fklubDialect')
+
+oldprice_file_handle = open('fklubdw/FKlubSourceData/oldprice.csv')
+oldprice_source = TypedCSVSource(oldprice_file_handle,
+                                 {'id': int, 'product_id': int,
+                                  'price': int, 'changed_on': datetime.datetime},
+                                 dialect='fklubDialect')
+
+payment_file_handle = open('fklubdw/FKlubSourceData/payment.csv')
+payment_source = TypedCSVSource(payment_file_handle,
+                                {'id': int, 'member_id': int,
+                                 'timestamp': datetime.datetime, 'amount': int}, dialect='fklubDialect')
+
+product_categories_file_handle = open(
+    'fklubdw/FKlubSourceData/product_categories.csv')
+product_categories_source = TypedCSVSource(product_categories_file_handle,
+                                           {'id': int, 'product_id': int,
+                                            'category_id': int},
+                                           dialect='fklubDialect')
+
+product_file_handle = open('fklubdw/FKlubSourceData/product.csv')
+product_source = TypedCSVSource(product_file_handle,
+                                {'id': int, 'name': str, 'price': int,
+                                 'active': bool, 'deactivate_date': datetime.datetime,
+                                 'quantity': int, 'alcohol_content_ml': float,
+                                 'start_date': datetime.datetime},
+                                dialect='fklubDialect')
+
+store_file_handle = open('fklubdw/FKlubSourceData/room.csv')
+store_source = TypedCSVSource(store_file_handle,
+                              {'id': int, 'name': str, 'description': str},
+                              dialect='fklubDialect')
+
+sale_file_handle = open('fklubdw/FKlubSourceData/sale.csv')
+sale_source = TypedCSVSource(sale_file_handle,
+                             {'id': str, 'member_id': int,
+                              'product_id': int, 'room_id': int,
+                              'timestamp': datetime.datetime, 'price': int},
+                             dialect='fklubDialect')
+
+
+### Dimensions and Facts ###
+
+product_dimension = SlowlyChangingDimension(
+    name="dim.product",
+    key="product_id",
+    attributes=["name", "category", "price", "alcohol_content_ml",
+                "activate_date", "deactivate_date", "version",
+                "valid_from", "valid_to"],
+    lookupatts=["name"],
+    versionatt="version",
+    fromatt="valid_from",
+    toatt="valid_to",
+    srcdateatt="lastmoddate",
+    cachesize=-1
 )
 
 
-timeDim = CachedDimension(
+time_dimension = CachedDimension(
     name='dim.time',
     key='time_id',
-    attributes=['t_date', 't_year', 't_month', 't_day', 't_hour', 
-    'day_of_week', 'fall_semester', 'is_holiday'],
-    lookupatts="t_date"
-    )
-
-storeDim = SlowlyChangingDimension(
-  name="dim.store",
-  key="store_id",
-  attributes=["city", "building", "room", "version", "valid_from", "valid_to"],
-  lookupatts="building", # Should be something unique
-  versionatt="version",
-  fromatt="valid_from",
-  toatt="valid_to",
-  srcdateatt="lastmoddate",
-  cachesize=-1
+    attributes=['t_date', 't_year', 't_month', 't_day', 't_hour',
+                'day_of_week', 'is_fall_semester', 'is_holiday'],
+    lookupatts=["t_year", "t_month", "t_day", "t_hour"]
+    #rowexpander=timehandling TODO
 )
 
+store_dimension = SlowlyChangingDimension(
+    name="dim.store",
+    key="store_id",
+    attributes=["name", "description",
+                "version", "valid_from", "valid_to"],
+    lookupatts=["name"],
+    versionatt="version",
+    fromatt="valid_from",
+    toatt="valid_to",
+    srcdateatt="lastmoddate",
+    cachesize=-1
+)
+
+member_dimension = SlowlyChangingDimension(
+    name="dim.member",
+    key="member_id",
+    attributes=["gender", "is_active", "course",
+                "version", "valid_from", "valid_to"],
+    lookupatts=["member_id"],
+    versionatt="version",
+    fromatt="valid_from",
+    toatt="valid_to",
+    srcdateatt="lastmoddate",
+    cachesize=-1
+)
+
+# TODO: Use BulkFactTable or BatchFactTable
+fact_table = FactTable(
+    name="fct.sale",
+    keyrefs=["fk_product_id", "fk_time_id", "fk_store_id", "fk_member_id"],
+    measures=["order_id", "item_count"]
+)
+
+### Dimension Filling ###
+
+# Store
+
+# TODO
+def fill_product_dimension():
+    pass
+
+# TODO
+# We can fill the time dimension lazily using the rowexpander
+# Look at the datehandling function: https://chrthomsen.github.io/pygrametl/
+
+# TODO
+def fill_member_dimension():
+    pass
 
 
+def fill_store_dimension():
+    for srcrow in store_source:
+        # in: id, name, description
+        dimrow = {'id': srcrow['id'], 'name': srcrow['name'],
+                  'description': srcrow['description'],
+                  'version': 1, 'valid_from': datetime.date(1970, 1, 1),
+                  'valid_to': None}
+
+        # out: name, description, version, valid_from, valid_to=null
+        store_dimension.insert(dimrow)
+
+
+### Fact Filling ###
+# TODO
+def fill_fact_table():
+  pass
+
+### Main ###
+
+def main():
+    # TODO: automatic cleanup of the db
+    #executeScriptsFromFile('cleanAndCreate.sql')
+
+    # Dimension filling
+    fill_product_dimension()
+    fill_member_dimension()
+    fill_store_dimension()
+
+    # Fact filling
+    fill_fact_table()
+
+    connection.commit()
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
